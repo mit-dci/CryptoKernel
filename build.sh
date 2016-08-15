e2424a682f4a9d1cf2ca34e7ed8254e75b8ec083
unknown <jameslovejoy1@gmail.com>
2016-08-15 13:29:37 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
-g++ main.cpp crypto.cpp base64.cpp -o main -lcrypto
+#!/bin/sh
 
+CXX=g++
+CXXFLAGS=-Wall -std=c++14 -std=c++14 -O2 -fPIC
+
+${CXX} ${CXXFLAGS} -c blockchain.cpp -o blockchain.o
+${CXX} ${CXXFLAGS} -c base64.cpp -o base64.o
+${CXX} ${CXXFLAGS} -c crypto.cpp -o crypto.o
+${CXX} ${CXXFLAGS} -c log.cpp -o log.o
+${CXX} ${CXXFLAGS} -c math.cpp -o math.o
+${CXX} ${CXXFLAGS} -c network.cpp -o network.o
+${CXX} ${CXXFLAGS} -c storage.cpp -o storage.o
+ar -r -s libCryptoKernel.a base64.o blockchain.o crypto.o log.o math.o network.o storage.o
