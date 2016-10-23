08d1d7138378219362ba3946902560ffc59ac64d
James Lovejoy <jameslovejoy1@gmail.com>
2016-10-23 15:59:52 -0400

    Fix test build script to find libary headers

--- test.sh
+++ test.sh
@@ -4,8 +4,9 @@ exe() { echo "$@" ; "$@" ; }
 
 CXX=g++
 CXXFLAGS="-Wall -std=c++14 -O2 -fPIC"
+INCLUDES="-Isrc"
 
-exe ${CXX} ${CXXFLAGS} -c tests/CryptoKernelTestRunner.cpp -o tests/CryptoKernelTestRunner.o
-exe ${CXX} ${CXXFLAGS} -c tests/CryptoTests.cpp -o tests/CryptoTests.o
-exe ${CXX} -o test tests/CryptoKernelTestRunner.o tests/CryptoTests.o -Isrc -L. -lCryptoKernel -lcrypto -pthread -ldl -lcppunit
+exe ${CXX} ${CXXFLAGS} ${INCLUDES} -c tests/CryptoKernelTestRunner.cpp -o tests/CryptoKernelTestRunner.o
+exe ${CXX} ${CXXFLAGS} ${INCLUDES} -c tests/CryptoTests.cpp -o tests/CryptoTests.o
+exe ${CXX} -o test tests/CryptoKernelTestRunner.o tests/CryptoTests.o -L. -lCryptoKernel -lcrypto -pthread -ldl -lcppunit
 ./test
