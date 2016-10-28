cf4f40a81cf2cb00ec8abd9088fff6bc732c6387
James Lovejoy <jameslovejoy1@gmail.com>
2016-10-27 22:26:00 -0400

    Document crypto class.

--- src/crypto.h
+++ src/crypto.h
@@ -26,19 +26,87 @@
 namespace CryptoKernel
 {
 
+/**
+* A mutable class which performs cryptography operations on ECDSA (secp256k1) keypairs. It provides funtions to
+* generate keys, signing and verifying. It also provides a static function for generating SHA256 hashes. 
+*/
 class Crypto
 {
 public:
-    Crypto(bool fGenerate = false);
+    
+    /**
+    * Constructs a crypto object. Optionally generates a secp256k1 keypair.
+    *
+    * @param fGenerate optional param, when set to true, generates a secp256k1 keypair. Otherwise if false or ommited, do not generate.
+    */
+    Crypto(const bool fGenerate = false);
+
+    /**
+    * Crypto class deconstructor
+    */
     ~Crypto();
+
+    /**
+    * Returns the status of the crypto module
+    *
+    * @return the status of the crypto module. True if no errors have occured, False otherwise.
+    */
     bool getStatus();
-    std::string sign(std::string message);
-    bool verify(std::string message, std::string signature);
+    
+    /**
+    * Generates an ECDSA signature from the given message using a SHA256 digest.
+    *
+    * @param message the message to sign, must be non-empty
+    * @return the ECDSA signature of the message with a SHA256 digest 
+    */
+    std::string sign(const std::string message);
+        
+    /**
+    * Verifies a that a given signature verifies the given message with the public key of this class
+    *
+    * @param message the message to verify, must be non-empty
+    * @param signature the signature to check against, must be non-empty
+    * @return true if the signature is correctly verified against the message and public key, false otherwise
+    */
+    bool verify(const std::string message, const std::string signature);
+
+    /**
+    * Returns the public key of the instance
+    * 
+    * @return the public key of the class encoded base64
+    */
     std::string getPublicKey();
+
+    /**
+    * Returns the private key of the instance
+    *
+    * @return the private key of the class encoded base64
+    */
     std::string getPrivateKey();
-    bool setPublicKey(std::string publicKey);
-    bool setPrivateKey(std::string privateKey);
-    std::string sha256(std::string message);
+
+    /**
+    * Sets the public key of the instance
+    *
+    * @param publicKey valid public key from another instance of Crypto
+    * @return true if setting the key was successful, false otherwise
+    */
+    bool setPublicKey(const std::string publicKey);
+
+    /**
+    * Sets the private key of the instance
+    *
+    * @param privateKey valid private key from another instance of Crypto
+    * @return true if setting the key was successful, false otherwise
+    */
+    bool setPrivateKey(const std::string privateKey);
+    
+    /**
+    * Static function which generates a hex encoded SHA256 hash of the given message
+    *
+    * @param message the message to hash, must be non-empty
+    * @return the SHA256 hash of the message, hex encoded, lowercase 
+    */
+    static std::string sha256(const std::string message);
 
 private:
     bool status;
