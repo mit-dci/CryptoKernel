1abbce804f59959c796b3b0a50d189c61b1d3bf9
James Lovejoy <jameslovejoy1@gmail.com>
2016-11-04 19:58:41 -0400

    Fix divide test

--- tests/MathTests.cpp
+++ tests/MathTests.cpp
@@ -53,5 +53,9 @@ void MathTest::testDivide() {
     const std::string second = "bAc391045cEE3Dfe";
     const std::string expected = "1";
 
-    const std::string actual = CryptoKernel::Math::divideHex(second, first);
+    std::string actual = CryptoKernel::Math::divideHex(second, first);
+    
+    actual.erase(0, std::min(actual.find_first_not_of('0'), actual.size()-1));
+
+    CPPUNIT_ASSERT_EQUAL(expected, actual);
 }
