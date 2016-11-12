8323f4523486280d26b13d7e4df0966f2c8659c5
James Lovejoy <jameslovejoy1@gmail.com>
2016-11-11 23:09:16 -0500

    Write a test for the log module
    At the moment this just tests the expected return values of the log class' functions. In the future this should be expanded to check the values written to the log file for correctness.

--- test.sh
+++ test.sh
@@ -11,5 +11,6 @@ exe ${CXX} ${CXXFLAGS} ${INCLUDES} -c tests/CryptoKernelTestRunner.cpp -o tests/
 exe ${CXX} ${CXXFLAGS} ${INCLUDES} -c tests/CryptoTests.cpp -o tests/CryptoTests.o
 exe ${CXX} ${CXXFLAGS} ${INCLUDES} -c tests/MathTests.cpp -o tests/MathTests.o
 exe ${CXX} ${CXXFLAGS} ${INCLUDES} -c tests/StorageTests.cpp -o tests/StorageTests.o
-exe ${CXX} -o test tests/CryptoKernelTestRunner.o tests/StorageTests.o tests/CryptoTests.o tests/MathTests.o -L. ${LIBS} 
+exe ${CXX} ${CXXFLAGS} ${INCLUDES} -c tests/LogTests.cpp -o tests/LogTests.o
+exe ${CXX} -o test tests/CryptoKernelTestRunner.o tests/StorageTests.o tests/CryptoTests.o tests/MathTests.o tests/LogTests.o -L. ${LIBS} 
 ./test
---
+++ tests/LogTests.h
@@ -0,0 +1,25 @@
+#ifndef LOGTEST_H
+#define	LOGTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "log.h"
+
+class LogTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(LogTest);
+
+    CPPUNIT_TEST(testLogging);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    LogTest();
+    virtual ~LogTest();
+    void setUp();
+    void tearDown();
+
+private:
+    void testLogging();
+};
+
+#endif
