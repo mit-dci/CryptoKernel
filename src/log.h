46310c23ffd1f1d33fb5ff3f2f37b32dac653ec2
James Lovejoy <jameslovejoy1@gmail.com>
2016-11-11 22:35:35 -0500

    Write doxygen documentation comments for the log class

--- src/log.h
+++ src/log.h
@@ -29,12 +29,54 @@
 namespace CryptoKernel
 {
 
+/**
+* The log class provides basic logging capabilities including printing messages
+* to the screen and write to a log file. Automatically adds timestamps and 
+* message severity level.
+*/
 class Log
 {
     public:
-        Log(std::string filename = "CryptoKernel.log", bool printToConsole = false);
+        /**
+        * Constructs a log instance using the given filename. Optionally prints
+        * log messages to the console. This will either create the log file or 
+        * lock the existing log file for writing and add newlines to the file 
+        * before starting the log, continuing from the last line already in the
+        * log file.
+        *
+        * @param filename optional filename of the log file. Uses "CryptoKernel.log" by default
+        * @param printToConsole optional flag, prints log messages to the console when true.
+                 Defaults to false.
+        */
+        Log(const std::string filename = "CryptoKernel.log", const bool printToConsole = false);
+        
+        /**
+        * Standard destructor. Should ensure messages are flushed to the log file and
+        * release the file for writing by other processes.
+        */
         ~Log();
-        bool printf(int loglevel, std::string message);
+
+        /**
+        * Adds a message to the log with a given severity, leaving a new line in the 
+        * following format:
+        *
+        * YYYY-MM-DD HH:MM:SS ERROR|WARNING|INFO Log message
+        *
+        * If the print to console flag was set to true in the constructor the message
+        * is also printed to the console.
+        *
+        * @param loglevel the severity of the message, can either be LOG_LEVEL_ERR (1), LOG_LEVEL_WARN (2)
+        *        or LOG_LEVEL_INFO (3)
+        * @param message the message string of the log entry
+        * @return true iff the message was logged successfully, otherwise false
+        */
+        bool printf(const int loglevel, const std::string message);
+
+        /**
+        * Checks the status of the log class
+        *
+        * @return true iff there are no internal errors and it is safe to continue
+        */
         bool getStatus();
 
     private:
