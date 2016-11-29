d52f282bdea7f4ac36d4e5f18b9c74c107e0556a
James Lovejoy <jameslovejoy1@gmail.com>
2016-11-29 15:37:50 -0500

    Write header specification for ContractRunner class

---
+++ src/kernel/contract.h
@@ -0,0 +1,55 @@
+#ifndef CONTRACT_H_INCLUDED
+#define CONTRACT_H_INCLUDED
+
+#include "blockchain.h"
+
+namespace CryptoKernel
+{
+    /**
+    * This class runs Lua smart contracts inside transaction inputs to verify validity.
+    * Provides methods to evaluate a transactions validity, compile contracts to bytecode
+    * and limit execution resources
+    */
+    class ContractRunner
+    {
+        public:
+            /**
+            * Default constructor, creates a ContractRunner instance with access to the given blockchain
+            * and with the given resource and execution limits
+            *
+            * @param blockchain a pointer to the blockchain object to be used for reference
+            * @param memoryLimit specify the memory limit in bytes of the virtual machine, defaults to 1MB
+            * @param instructionLimit specify the maximum number of instructions a contract can execute, defaults to 35000
+            */
+            ContractRunner(const CryptoKernel::Blockchain* blockchain, const uint64_t memoryLimit = 1048576, const uint64_t instructionLimit = 35000);
+
+            /**
+            * Default destructor
+            */
+            ~ContractRunner();
+
+            /**
+            * Static method for compiling a contract into bytecode such that it can be used in a
+            * transaction. Takes the function "verify" in the given script source and returns
+            * the LVM bytecode compressed with lz4.
+            *
+            * @param contractScript a valid CryptoKernel contract script
+            * @return the bytecode of the script compressed with lz4
+            */
+            static std::string compile(const std::string contractScript);
+
+            /**
+            * Evaluate all of the input scripts in the given transaction to determine if the transaction
+            * is valid according to the contract rules
+            *
+            * @param tx the transaction to be verified
+            * @return true iff the transaction is valid according to contract scripts, false otherwise
+            */
+            bool evaluateValid(const transaction tx);
+
+        private:
+            void load(const std::string bytecode);
+    };
+}
+
+#endif // CONTRACT_H_INCLUDED
