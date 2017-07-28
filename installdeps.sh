a790289313cba5f6b47aaf2007b74de87fa70005
James Lovejoy <jameslovejoy1@gmail.com>
2017-07-28 18:17:56 -0400

    Update install dependencies script

--- installdeps.sh
+++ installdeps.sh
@@ -1,10 +1,26 @@
 sudo apt-get update
-sudo apt-get install -y git build-essential libjsoncpp-dev libsfml-dev libleveldb-dev libreadline-dev libcurl4-gnutls-dev libmicrohttpd-dev libjsonrpccpp-dev liblua5.3-dev
+sudo apt-get install -y git build-essential libjsoncpp-dev libsfml-dev libleveldb-dev libargtable2-dev libreadline-dev libcurl4-gnutls-dev liblua5.3-dev cmake
 
 wget https://www.openssl.org/source/openssl-1.1.0f.tar.gz
 tar -xvzf openssl-1.1.0f.tar.gz
 cd openssl-1.1.0f
 ./config
+make
+sudo make install
+cd ../
+
+wget http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-latest.tar.gz
+tar -xvzf libmicrohttpd-latest.tar.gz
+cd libmicrohttpd*
+./configure
+make
+sudo make install
+cd ../
+
+wget https://github.com/cinemast/libjson-rpc-cpp/archive/v0.7.0.tar.gz
+tar -xvzf v0.7.0.tar.gz
+cd libjson-rpc-cpp*
+cmake .
 make
 sudo make install
 
