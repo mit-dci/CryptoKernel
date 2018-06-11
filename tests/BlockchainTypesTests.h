bcdeefb6aa06d411edcefbac8851777f5754630c
James Lovejoy <jameslovejoy1@gmail.com>
2018-06-11 14:04:38 -0400

    FastWriter used to add a newline to output, new code should do the same (#33)
    When we replaced FastWriter with StreamWriterBuilder the output format changed (neglecting a trailing newline) and thus blockchain type IDs changed. Since that's consensus critical the new toString code needs to produce the same format string. Add a unit test to catch regressions.

---
+++ tests/BlockchainTypesTests.h
@@ -0,0 +1,24 @@
+#ifndef CRYPTOTEST_H
+#define CRYPTOTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class BlockchainTypesTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(BlockchainTypesTest);
+
+    CPPUNIT_TEST(testOutputId);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    BlockchainTypesTest();
+    virtual ~BlockchainTypesTest();
+    void setUp();
+    void tearDown();
+
+private:
+    void testOutputId();
+
+};
+
+#endif
