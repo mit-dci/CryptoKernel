10ea38358a6ebac2f80ca4776913a6460dde5269
James Lovejoy <jameslovejoy1@gmail.com>
2018-06-24 22:30:59 -0400

    Add premake5 script and remove un-used raft files

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+workspace "CryptoKernel"
+    configurations {"Debug", "Release"}
+    platforms {"Static", "Shared"}
+    language "C++"
+    cppdialect "C++14"
+    includedirs {"/usr/include/lua5.3", "src/kernel"}
+
+    symbols "On"
+
+    filter {"configurations:Debug"}
+        optimize "Off"
+
+    filter {"configurations:Release"}
+        optimize "Full"
+
+cklibs = {"crypto", "lua5.3", "sfml-network", 
+"sfml-system", "leveldb", "jsoncpp", "jsonrpccpp-server", 
+"jsonrpccpp-client", "jsonrpccpp-common", "microhttpd"}
+
+project "ck"
+        
+    files {"src/kernel/**.cpp", "src/kernel/**.h", "src/kernel/**.c"}
+    
+    filter {"platforms:Static"}
+        kind "StaticLib"
+
+    filter {"platforms:Shared"}
+        kind "SharedLib"
+
+project "ckd"
+
+    kind "ConsoleApp"
+    files {"src/client/**.cpp", "src/client/**.h"}
+    links {"ck"}
+    links(cklibs)
+
+    filter "system:linux"
+        links {"pthread"}
+
+project "test"
+
+    kind "ConsoleApp"
+    files {"tests/**.cpp", "tests/**.h"}
+    links {"ck", "cppunit"}
+    links(cklibs)
+    postbuildcommands{"%{cfg.linktarget.abspath}"}
