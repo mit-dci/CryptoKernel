f9c2f71acd342d03530158e8a7b46ac941cdca53
James Lovejoy <jameslovejoy1@gmail.com>
2018-07-02 12:14:10 -0400

    Swap manual makefile for premake5 (#39)
    * Add premake5 script and remove un-used raft files
    
    * Add docs build
    
    * Support build options
    
    * Apply lib-dir and include-dir to all projects
    
    * Windows library names are different from linux
    
    * More libs and a decl needed for windows
    
    * Yet more libs for windows
    
    * Windows cross-compile working via dockerfile with premake5
    
    * Add OSX specific libs
    
    * OSX cross-compile build completes with premake5
    
    * Correct path for dylibbundler to use
    
    * Linux static build with Dockerfile and premake5 working
    
    * Move system specific library link commands to a function
    
    * Remove custom Makefile, update installdeps.sh and readme
    
    * Linux build requires gcov

---
+++ premake5.lua
@@ -0,0 +1,86 @@
+newoption {
+    trigger     = "with-docs",
+    description = "Use doxygen to build the docs"
+}
+ 
+newoption {
+    trigger = "lib-dir",
+    description = "Specify extra directory to look for libraries in",
+    value = "DIR"
+}
+
+newoption {
+    trigger = "include-dir",
+    description = "Specify extra directory to look for headers in",
+    value = "DIR"
+}
+
+workspace "CryptoKernel"
+    configurations {"Debug", "Release"}
+    platforms {"Static", "Shared"}
+    language "C++"
+    cppdialect "C++14"
+    includedirs {"src/kernel",
+                 _OPTIONS["include-dir"]}
+    libdirs {_OPTIONS["lib-dir"]}
+    symbols "On"
+
+    filter {"configurations:Debug"}
+        optimize "Off"
+
+    filter {"configurations:Release"}
+        optimize "Full"
+
+cklibs = {"crypto", "sfml-network", 
+"sfml-system", "leveldb", "jsoncpp", "jsonrpccpp-server", 
+"jsonrpccpp-client", "jsonrpccpp-common", "microhttpd"}
+
+linuxLinks = {"pthread", "lua5.3", "curl", "dl", "gcov"}
+
+winLinks = {"lua", "curl", "ws2_32", "shlwapi", "crypt32"}
+
+macLinks = {"lua", "curl"}
+
+function linkSystemSpecific()
+    filter "system:linux"
+        links(linuxLinks)
+
+    filter "system:windows"
+        links(winLinks)
+
+    filter "system:macosx"
+        links(macLinks)
+end
+
+project "ck"
+        
+    files {"src/kernel/**.cpp", "src/kernel/**.h", "src/kernel/**.c"}
+    
+    configuration "with-docs"
+        postbuildcommands{"doxygen %{wks.location}/doxyfile"}
+    
+    filter {"platforms:Static"}
+        kind "StaticLib"
+        defines {"SFML_STATIC"}
+
+    filter {"platforms:Shared"}
+        kind "SharedLib"
+
+project "ckd"
+
+    kind "ConsoleApp"
+    files {"src/client/**.cpp", "src/client/**.h"}
+    links {"ck"}
+    links(cklibs)
+
+    linkSystemSpecific()
+
+project "test"
+
+    kind "ConsoleApp"
+    files {"tests/**.cpp", "tests/**.h"}
+    links {"ck", "cppunit"}
+    links(cklibs)
+    postbuildcommands{"%{cfg.linktarget.abspath}"}
+
+    linkSystemSpecific()
