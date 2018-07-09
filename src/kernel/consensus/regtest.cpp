9bd04e23e2e6f6b87b054ae955b0f98f1b9706cd
James Lovejoy <jameslovejoy1@gmail.com>
2018-07-09 18:54:43 -0400

    Regtest consensus mode needs to use random nonces with coinbase outputs or there will be colliding txids when submiting blocks quickly in unit tests

--- src/kernel/consensus/regtest.cpp
+++ src/kernel/consensus/regtest.cpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include <math.h>
+#include <random>
 
 #include "regtest.h"
 #include "../crypto.h"
@@ -67,7 +68,15 @@ void CryptoKernel::Consensus::Regtest::mineBlock(const bool isBetter, const std:
 	Json::Value consensusData = Block.getConsensusData();
 	consensusData["isBetter"] = isBetter;
 	Block.setConsensusData(consensusData);
-	blockchain->submitBlock(Block);
+
+    std::random_device r;
+    std::default_random_engine generator(r());
+    std::uniform_int_distribution<uint64_t> distribution(0, std::numeric_limits<uint64_t>::max());
+
+	auto blockJson = Block.toJson();
+	blockJson["coinbaseTx"]["outputs"][0]["nonce"] = distribution(generator);
+		
+	blockchain->submitBlock(CryptoKernel::Blockchain::block(blockJson));
 }
 
 CryptoKernel::Consensus::Regtest::consensusData
