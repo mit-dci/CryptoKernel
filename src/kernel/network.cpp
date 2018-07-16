b7007af27330ba47db55bb07cef4bbf6d5064cb7
Luke Horgan <horgan.l@husky.neu.edu>
2018-07-16 16:22:48 -0400

    enigma

--- src/kernel/network.cpp
+++ src/kernel/network.cpp
@@ -204,8 +204,9 @@ void CryptoKernel::Network::makeOutgoingConnections(bool& wait) {
 	std::vector<std::string> peerIps;
 
 	std::unique_ptr<Storage::Transaction> dbTx(networkdb->beginReadOnly());
-	std::unique_ptr<CryptoKernel::Storage::Table::Iterator> it(
-			new CryptoKernel::Storage::Table::Iterator(peers.get(), networkdb.get(), dbTx->snapshot));
+	//std::unique_ptr<CryptoKernel::Storage::Table::Iterator> it(new CryptoKernel::Storage::Table::Iterator(peers.get(), networkdb.get(), dbTx->snapshot));
+
+	std::unique_ptr<Storage::Table::Iterator> it(new Storage::Table::Iterator(peers.get(), networkdb.get(), dbTx->snapshot));
 
 	it->snapshot;
 
