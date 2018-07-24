a48ad2941e3cbdea62fbbd07a168c7d542a076e1
Luke Horgan <horgan.l@husky.neu.edu>
2018-07-24 16:13:28 -0400

    herhasd

--- src/kernel/network.cpp
+++ src/kernel/network.cpp
@@ -262,9 +262,9 @@ void CryptoKernel::Network::incomingEncryptionHandshakeFunc() {
 	                    	packet >> message;
 	                    	log->printf(LOG_LEVEL_INFO, "received message " + message);
 	                    }*/
-	                	log->printf(LOG_LEVEL_INFO, "starting ncc");
-	                	NoiseConnectionClient ncc(&client, log);
-	                	ncc.execHandshake();
+	                	log->printf(LOG_LEVEL_INFO, "starting ncs");
+	                	NoiseConnectionServer ncs(&client, log);
+	                	ncs.execHandshake();
 	                }
 	            }
 	        }
@@ -298,9 +298,9 @@ void CryptoKernel::Network::outgoingEncryptionHandshakeFunc() {
 				it++;
 				// todo, check age of socket, and remove it from the list after three seconds
 			}*/
-			log->printf(LOG_LEVEL_INFO, "starting ncs");
-			NoiseConnectionServer ncs(it->second, log);
-			ncs.execHandshake();
+			log->printf(LOG_LEVEL_INFO, "starting ncc");
+			NoiseConnectionClient ncc(it->second, log);
+			ncc.execHandshake();
 			pendingConnections.erase(it++);
 		}
 
