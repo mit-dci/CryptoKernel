131fd94573096f7adfff01b4d616b7b8c864e66c
Luke Horgan <horgan.l@husky.neu.edu>
2018-07-27 11:57:48 -0400

    hypothetically, encrypted packets are now encrypted packets

--- src/kernel/EncryptedPacket.h
+++ src/kernel/EncryptedPacket.h
@@ -9,17 +9,36 @@
 #define SRC_KERNEL_ENCRYPTEDPACKET_H_
 
 #include <SFML/Network.hpp>
+#include <noise/protocol.h>
 
 class EncryptedPacket : public sf::Packet {
+public:
+	NoiseCipherState* send_cipher;
+	NoiseCipherState* recv_cipher;
+
+public:
     virtual const void* onSend(std::size_t& size) {
-        const void* srcData = getData();
+    	NoiseBuffer mbuf;
+
+    	const void* srcData = getData();
         std::size_t srcSize = getDataSize();
-        return srcData;
+
+        noise_buffer_set_inout(mbuf, (uint8_t*)srcData, srcSize, 4096);
+        int err = noise_cipherstate_encrypt(send_cipher, &mbuf);
+
+        size = mbuf.size;
+        return mbuf.data;
     }
 
     virtual void onReceive(const void* data, std::size_t size) {
-        std::size_t dstSize;
-        append(data, dstSize);
+    	NoiseBuffer mbuf;
+
+    	std::size_t dstSize;
+
+    	noise_buffer_set_inout(mbuf, (uint8_t*)data, dstSize, 4096);
+		int err = noise_cipherstate_decrypt(recv_cipher, &mbuf);
+
+        append(mbuf.data, mbuf.size);
     }
 };
 
