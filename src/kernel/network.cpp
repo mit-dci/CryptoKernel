9ed504698b0a15a1a8b7fa315d5f47f309957d3d
Luke Horgan <horgan.l@husky.neu.edu>
2018-08-06 13:13:29 -0400

    up wait time

--- src/kernel/network.cpp
+++ src/kernel/network.cpp
@@ -367,7 +367,7 @@ void CryptoKernel::Network::makeOutgoingConnectionsWrapper() {
 			std::this_thread::sleep_for(std::chrono::seconds(20)); // stop looking for a while
 		}
 		else {
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			std::this_thread::sleep_for(std::chrono::seconds(2));
 		}
 	}
 }
