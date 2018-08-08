a4baa8d45d3c8f9bfa034e6c5a4d7b2a7f0e1d61
Luke Horgan <horgan.l@husky.neu.edu>
2018-08-08 14:40:13 -0400

    get things in order

--- go.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-make
-cp bin/Static/Debug/ckd ckd
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -39,198 +39,182 @@
 
 bool running;
 
-void startup(int argc, char* argv[]);
-
 int main(int argc, char* argv[]) {
-	startup(argc, argv);
+    std::ifstream t("config.json");
+    if(!t.is_open()) {
+        throw std::runtime_error("Could not open config file");
+    }
 
-	/*std::cout << "HELLO WORLD" << std::endl;
-	Json::Value yay;
-	yay = "hello";
+    std::string buffer((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+    Json::Value config = CryptoKernel::Storage::toJson(buffer);
 
-	std::string password = "hello";
-	std::string plaintext = "yaylife";
+    t.close();
 
-	CryptoKernel::AES256 nifty(password, plaintext);
-	std::cout << nifty.decrypt("hello") << std::endl;*/
+    if(config["rpcpassword"] == "password") {
+        unsigned char bytes[32];
+        if(!RAND_bytes((unsigned char*)&bytes, 32)) {
+            throw std::runtime_error("Couldn't generate random rpc password");
+        }
 
-	return 0;
-}
+        config["rpcpassword"] = base64_encode((unsigned char*)&bytes, 32);
 
-void startup(int argc, char* argv[]) {
-	std::ifstream t("config.json");
-	if(!t.is_open()) {
-		throw std::runtime_error("Could not open config file");
-	}
+        std::ofstream ofs("config.json", std::ofstream::out | std::ofstream::trunc);
 
-	std::string buffer((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
-	Json::Value config = CryptoKernel::Storage::toJson(buffer);
+        ofs << config.toStyledString();
 
-	t.close();
+        ofs.close();
+    }
 
-	if(config["rpcpassword"] == "password") {
-		unsigned char bytes[32];
-		if(!RAND_bytes((unsigned char*)&bytes, 32)) {
-			throw std::runtime_error("Couldn't generate random rpc password");
-		}
+    if(argc < 2) {
+        CryptoKernel::Log log("CryptoKernel.log", config["verbose"].asBool());
 
-		config["rpcpassword"] = base64_encode((unsigned char*)&bytes, 32);
+        running = true;
+        std::signal(SIGINT, [](int signal) { running = false; });
 
-		std::ofstream ofs("config.json", std::ofstream::out | std::ofstream::trunc);
+        CryptoKernel::MulticoinLoader loader("./config.json", &log, &running);
 
-		ofs << config.toStyledString();
+        if(!config["verbose"].asBool()) {
+            std::cout << "ck daemon started" << std::endl;
+        }
 
-		ofs.close();
-	}
+        while(running) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
 
-	if(argc < 2) {
-		CryptoKernel::Log log("CryptoKernel.log", config["verbose"].asBool());
+        log.printf(LOG_LEVEL_INFO, "Shutting down...");
+    } else {
+        std::string command(argv[1]);
 
-		running = true;
-		std::signal(SIGINT, [](int signal) { running = false; });
+        std::string port = "8383";
 
-		CryptoKernel::MulticoinLoader loader("./config.json", &log, &running);
+        int offset = 0;
 
-		if(!config["verbose"].asBool()) {
-			std::cout << "ck daemon started" << std::endl;
-		}
-
-		while(running) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		}
-
-		log.printf(LOG_LEVEL_INFO, "Shutting down...");
-	} else {
-		std::string command(argv[1]);
-
-		std::string port = "8383";
-
-		int offset = 0;
-
-		if(command == "-p" && argc >= 4) {
-			command = std::string(argv[3]);
-			port = std::string(argv[2]);
-			offset = 2;
-		} else if(command == "-p") {
-			throw std::runtime_error("Malformed commands");
-		}
+        if(command == "-p" && argc >= 4) {
+            command = std::string(argv[3]);
+            port = std::string(argv[2]);
+            offset = 2;
+        } else if(command == "-p") {
+            throw std::runtime_error("Malformed commands");
+        }
 
 		const std::string userpass = config["rpcuser"].asString()
-								   + ":"
-								   + config["rpcpassword"].asString();
+                                   + ":"
+                                   + config["rpcpassword"].asString();
 		const std::string auth = base64_encode((unsigned char*)userpass.c_str(),
-											   userpass.size());
+		                                       userpass.size());
 
-		jsonrpc::HttpClient httpclient("http://127.0.0.1:" + port);
-		httpclient.SetTimeout(30000);
+        jsonrpc::HttpClient httpclient("http://127.0.0.1:" + port);
+        httpclient.SetTimeout(30000);
 		httpclient.AddHeader("Authorization", "Basic " + auth);
-		CryptoClient client(httpclient);
+        CryptoClient client(httpclient);
 
-		try {
-			if(command == "-daemon") {
-				#ifdef _WIN32
-				if(std::system("start /B .\\ckd") != 0) {
-				#else
-				if(std::system("./ckd &") != 0) {
-				#endif
-					std::cout << "Failed to start ckd" << std::endl;
-				}
-			} else if(command == "getinfo") {
-				std::cout << client.getinfo().toStyledString() << std::endl;
-			} else if(command == "account") {
-				if(argc >= 3 + offset) {
-					const std::string name(argv[2 + offset]);
-					const std::string password = getPass("Please enter your wallet passphrase: ");
-					std::cout << client.account(name, password).toStyledString() << std::endl;
-				} else {
-					std::cout << "Usage: account [accountname]" << std::endl;
-				}
-			} else if(command == "sendtoaddress") {
-				if(argc >= 4 + offset) {
-					const std::string address(argv[2 + offset]);
-					const double amount(std::strtod(argv[3 + offset], NULL));
-					const std::string password = getPass("Please enter your wallet passphrase: ");
-					std::cout << client.sendtoaddress(address, amount, password) << std::endl;
-				} else {
-					std::cout << "Usage: sendtoaddress [address] [amount]" << std::endl;
-				}
-			} else if(command == "listaccounts") {
-				std::cout << client.listaccounts().toStyledString() << std::endl;
-			} else if(command == "listunspentoutputs") {
-				if(argc == 3 + offset) {
-					std::string name(argv[2 + offset]);
-					std::cout << client.listunspentoutputs(name).toStyledString() << std::endl;
-				} else {
-					std::cout << "Usage: listunspentoutputs [accountname]" << std::endl;
-				}
-			} else if(command == "compilecontract") {
-				if(argc == 3 + offset) {
-					std::string code(argv[2 + offset]);
-					std::cout << client.compilecontract(code) << std::endl;
-				} else {
-					std::cout << "Usage: compilecontract [code]" << std::endl;
-				}
-			} else if(command == "listtransactions") {
-				std::cout << client.listtransactions().toStyledString() << std::endl;
-			} else if(command == "getblock") {
-				if(argc == 3 + offset) {
-					std::cout << client.getblock(std::string(argv[2 + offset])) << std::endl;
-				} else {
-					std::cout << "Usage: getblock [id]" << std::endl;
-				}
-			} else if(command == "getblockbyheight") {
-				if(argc == 3 + offset) {
-					std::cout << client.getblockbyheight(std::strtoull(argv[2 + offset], nullptr,
-														 0)).toStyledString() << std::endl;
-				} else {
-					std::cout << "Usage: getblockbyheight [height]" << std::endl;
-				}
-			} else if(command == "stop") {
-				std::cout << client.stop().toStyledString() << std::endl;
-			} else if(command == "importprivkey") {
-				if(argc >= 4 + offset) {
-					const std::string password = getPass("Please enter your wallet passphrase: ");
-					std::cout << client.importprivkey(std::string(argv[2 + offset]), std::string(argv[3 + offset]),
-													  password);
-				} else {
-					std::cout << "Usage: importprivkey [accountname] [privkey]" << std::endl;
-				}
-			} else if(command == "getpeerinfo") {
-				std::cout << client.getpeerinfo() << std::endl;
-			} else if(command == "gettransaction") {
-				if(argc == 3 + offset) {
-					std::cout << client.gettransaction(std::string(argv[2 + offset])).toStyledString() << std::endl;
-				} else {
-					std::cout << "Usage: gettransaction [id]" << std::endl;
-				}
-			} else if(command == "dumpprivkeys") {
-				if(argc >= 3 + offset) {
-					const std::string name(argv[2 + offset]);
-					const std::string password = getPass("Please enter your wallet passphrase: ");
-					std::cout << client.dumpprivkeys(name, password).toStyledString() << std::endl;
-				} else {
-					std::cout << "Usage: dumpprivkeys [accountname]" << std::endl;
-				}
-			} else {
-				std::cout << "CryptoKernel - Blockchain Development Toolkit - v" << version << "\n\n"
-						  << "[-p [port]]\n\n"
-						  << "account [accountname]\n"
-						  << "compilecontract [code]\n"
-						  << "dumpprivkeys [accountname]\n"
-						  << "getblock [id]\n"
-						  << "getblockbyheight [height]\n"
-						  << "getinfo\n"
-						  << "getpeerinfo\n"
-						  << "gettransaction [id]\n"
-						  << "importprivkey [accountname] [privkey]\n"
-						  << "listaccounts\n"
-						  << "listtransactions\n"
-						  << "listunspentoutputs [accountname]\n"
-						  << "sendtoaddress [address] [amount]\n"
-						  << "stop\n";
-			}
-		} catch(jsonrpc::JsonRpcException e) {
-			std::cout << e.what() << std::endl;
-		}
-	}
+        try {
+            if(command == "-daemon") {
+                #ifdef _WIN32
+                if(std::system("start /B .\\ckd") != 0) {
+                #else
+                if(std::system("./ckd &") != 0) {
+                #endif
+                    std::cout << "Failed to start ckd" << std::endl;
+                }
+            } else if(command == "getinfo") {
+                std::cout << client.getinfo().toStyledString() << std::endl;
+            } else if(command == "account") {
+                if(argc >= 3 + offset) {
+                    const std::string name(argv[2 + offset]);
+                    const std::string password = getPass("Please enter your wallet passphrase: ");
+                    std::cout << client.account(name, password).toStyledString() << std::endl;
+                } else {
+                    std::cout << "Usage: account [accountname]" << std::endl;
+                }
+            } else if(command == "sendtoaddress") {
+                if(argc >= 4 + offset) {
+                    const std::string address(argv[2 + offset]);
+                    const double amount(std::strtod(argv[3 + offset], NULL));
+                    const std::string password = getPass("Please enter your wallet passphrase: ");
+                    std::cout << client.sendtoaddress(address, amount, password) << std::endl;
+                } else {
+                    std::cout << "Usage: sendtoaddress [address] [amount]" << std::endl;
+                }
+            } else if(command == "listaccounts") {
+                std::cout << client.listaccounts().toStyledString() << std::endl;
+            } else if(command == "listunspentoutputs") {
+                if(argc == 3 + offset) {
+                    std::string name(argv[2 + offset]);
+                    std::cout << client.listunspentoutputs(name).toStyledString() << std::endl;
+                } else {
+                    std::cout << "Usage: listunspentoutputs [accountname]" << std::endl;
+                }
+            } else if(command == "compilecontract") {
+                if(argc == 3 + offset) {
+                    std::string code(argv[2 + offset]);
+                    std::cout << client.compilecontract(code) << std::endl;
+                } else {
+                    std::cout << "Usage: compilecontract [code]" << std::endl;
+                }
+            } else if(command == "listtransactions") {
+                std::cout << client.listtransactions().toStyledString() << std::endl;
+            } else if(command == "getblock") {
+                if(argc == 3 + offset) {
+                    std::cout << client.getblock(std::string(argv[2 + offset])) << std::endl;
+                } else {
+                    std::cout << "Usage: getblock [id]" << std::endl;
+                }
+            } else if(command == "getblockbyheight") {
+                if(argc == 3 + offset) {
+                    std::cout << client.getblockbyheight(std::strtoull(argv[2 + offset], nullptr,
+                                                         0)).toStyledString() << std::endl;
+                } else {
+                    std::cout << "Usage: getblockbyheight [height]" << std::endl;
+                }
+            } else if(command == "stop") {
+                std::cout << client.stop().toStyledString() << std::endl;
+            } else if(command == "importprivkey") {
+                if(argc >= 4 + offset) {
+                    const std::string password = getPass("Please enter your wallet passphrase: ");
+                    std::cout << client.importprivkey(std::string(argv[2 + offset]), std::string(argv[3 + offset]),
+                                                      password);
+                } else {
+                    std::cout << "Usage: importprivkey [accountname] [privkey]" << std::endl;
+                }
+            } else if(command == "getpeerinfo") {
+                std::cout << client.getpeerinfo() << std::endl;
+            } else if(command == "gettransaction") {
+                if(argc == 3 + offset) {
+                    std::cout << client.gettransaction(std::string(argv[2 + offset])).toStyledString() << std::endl;
+                } else {
+                    std::cout << "Usage: gettransaction [id]" << std::endl;
+                }
+            } else if(command == "dumpprivkeys") {
+                if(argc >= 3 + offset) {
+                    const std::string name(argv[2 + offset]);
+                    const std::string password = getPass("Please enter your wallet passphrase: ");
+                    std::cout << client.dumpprivkeys(name, password).toStyledString() << std::endl;
+                } else {
+                    std::cout << "Usage: dumpprivkeys [accountname]" << std::endl;
+                }
+            } else {
+                std::cout << "CryptoKernel - Blockchain Development Toolkit - v" << version << "\n\n"
+                          << "[-p [port]]\n\n"
+                          << "account [accountname]\n"
+                          << "compilecontract [code]\n"
+                          << "dumpprivkeys [accountname]\n"
+                          << "getblock [id]\n"
+                          << "getblockbyheight [height]\n"
+                          << "getinfo\n"
+                          << "getpeerinfo\n"
+                          << "gettransaction [id]\n"
+                          << "importprivkey [accountname] [privkey]\n"
+                          << "listaccounts\n"
+                          << "listtransactions\n"
+                          << "listunspentoutputs [accountname]\n"
+                          << "sendtoaddress [address] [amount]\n"
+                          << "stop\n";
+            }
+        } catch(jsonrpc::JsonRpcException e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    return 0;
 }
